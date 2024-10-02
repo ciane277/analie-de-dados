26e2beb86d7aa6904ebbfbd065fa204c31ab9b04
ciane277 <113988675+ciane277@users.noreply.github.com>
2024-10-02 16:57:14 -0300

    Add files via upload

---
+++ graficos/informacoesglobais.js
@@ -0,0 +1,6 @@
+const url= 'https://raw.githbusercontent.com/jakecianeemails/api/main/dados-globais.json'
+
+async function vizualizarinformacoesglobais() {
+    const res= await fetch(url)
+
+const}
